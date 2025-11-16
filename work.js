0acc20fb06c7c5cb3f54bab957ede13db172b042
ayoub <assiacodi@gmail.com>
2025-11-16 19:49:35 +0100

    i add deasign de agouter un emploiye and regex de formuler

---
+++ work.js
@@ -0,0 +1,15 @@
+let nav_bar = document.getElementById("right_bar")
+let input_name = document.getElementById("right_bar")
+let input_img_url = document.getElementById("right_bar")
+let inpunt_num_tele = document.getElementById("right_bar")
+
+function validation_formulair() {
+    let email_regex =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let number_regex =/^\+?[1-9]\d{1,14}$/
+    let nome_regex =/^[A-Za-z]+(?: [A-Za-z]+)*$/
+
+
+    
+}
+
+
